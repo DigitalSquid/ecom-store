d6326c3b73255acbdcc781189b6cddee26561e77
Kevin Robertson <contact@digitalsquid.net>
2020-03-14 17:42:51 +0000

    added firebase utils and user sign-up

--- src/pages/customer-access/customer-access.component.jsx
+++ src/pages/customer-access/customer-access.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./customer-access.styles.scss";
 
 const customerAccessPage = () => (
   <div className="customer-access">
     <SignIn />
+    <SignUp />
   </div>
 );
 
